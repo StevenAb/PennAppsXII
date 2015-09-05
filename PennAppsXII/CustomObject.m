0db33ea038b24e6fb1d7acf967a4aaeafa933db7
StevenAb <shadwclaw@gmail.com>
2015-09-05 15:20:37 -0400

    Set up
    Added CocoaPods, Parse

---
+++ PennAppsXII/CustomObject.m
@@ -0,0 +1,9 @@
+//
+//  CustomObject.m
+//  PennAppsXII
+//
+//  Created by Apple on 9/5/15.
+//  Copyright (c) 2015 #ilovelin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'Parse'
+pod 'Alamofire', '~> 1.3'
+pod 'SwiftyJSON', '~> 2.2.1'
